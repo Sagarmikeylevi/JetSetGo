c42b03bf6b2cf0edd40ab34a8afc745f4394519e
unknown <sagarmikeylevi@gmail.com>
2023-10-06 13:29:01 +0530

    completed user registation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage, { action as registerAction } from "./pages/RegisterPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+        action: registerAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <LandingPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { redirect } from "react-router-dom";
+import Register from "../components/Register";
+
+const RegisterPage = () => {
+  return <Register />;
+};
+
+export default RegisterPage;
+
+export const action = async ({ request }) => {
+  try {
+    const data = await request.formData();
+
+    const userData = {
+      name: data.get("name"),
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+
+    await axios.post("http://localhost:8000/api/user/register", userData);
+
+    return redirect("/login");
+  } catch (error) {
+    return error.response;
+  }
+};
