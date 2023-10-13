584f4f207804f3e0433709f5bc11c2ea07db236a
unknown <sagarmikeylevi@gmail.com>
2023-10-13 20:58:56 +0530

    completed the UI of flight page

--- src/components/Flight.jsx
+++ src/components/Flight.jsx
@@ -1,45 +1,115 @@
 import Card from "./UI/Card";
 import flight_icon from "../assets/flight_icon.png";
-import { FaCalendar, FaUser, FaSearch } from "react-icons/fa";
+import { FaCalendar, FaUser, FaSearch, FaCaretDown } from "react-icons/fa";
 import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Flight = () => {
-  const [destinationForm, setDestinationForm] = useState(true);
+  const [openDestination, setOpenDestination] = useState(false);
+  const [destinationQuery, setDestinationQuery] = useState("");
+  const [isDepartureDest, setIsDepartureDest] = useState(false);
+  const [isArrivalDest, setIsArrivalDest] = useState(false);
+  const [departureDest, setDepartureDest] = useState("New Delhi");
+  const [arrivalDest, setArrivalDest] = useState("Mumbai");
+  const [departureDate, setDepartureDate] = useState(new Date());
+  const [selectedClass, setSelectedClass] = useState("Economy");
+  const [flightIcon, setFlightIcon] = useState("");
+
+  const chooseDestinationHandler = (city) => {
+    if (isDepartureDest) setDepartureDest(city);
+    if (isArrivalDest) setArrivalDest(city);
+    setTimeout(() => {
+      setOpenDestination(false);
+      setIsDepartureDest(false);
+      setIsArrivalDest(false);
+    }, 200);
+  };
+
+  const destinationHandler = (way) => {
+    setTimeout(() => {
+      if (way.toLowerCase() === "from") {
+        setIsDepartureDest(true);
+        setFlightIcon("https://cdn-icons-png.flaticon.com/128/192/192180.png");
+      } else {
+        setIsArrivalDest(true);
+        setFlightIcon(
+          "https://cdn-icons-png.flaticon.com/128/3436/3436569.png"
+        );
+      }
+
+      setOpenDestination(true);
+    }, 200);
+  };
+
+  const demoSuggestions = [
+    {
+      city: "Kolkata",
+      airport: "Netaji Subhash Chandra Bose International Airport",
+    },
+    {
+      city: "Mumbai",
+      airport: "Chhatrapati Shivaji International Airport",
+    },
+  ];
+
+  const filteredSuggestions = demoSuggestions.filter((suggestion) =>
+    suggestion.city.toLowerCase().includes(destinationQuery.toLocaleLowerCase())
+  );
+
   const flightDetails = [
     {
       id: 1,
       icon: "https://cdn-icons-png.flaticon.com/128/192/192180.png",
       way: "From",
-      city: "New Delhi",
+      city: departureDest,
       country: "India",
     },
     {
       id: 2,
       icon: "https://cdn-icons-png.flaticon.com/128/3436/3436569.png",
       way: "To",
-      city: "Mumbai",
+      city: arrivalDest,
       country: "India",
     },
   ];
   return (
     <>
-      {destinationForm && (
+      {openDestination && (
         <div className="h-[25rem] w-[25rem] z-[999] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-4">
           <input
             type="text"
             placeholder="Search Destination"
             className="w-full px-4 py-2 rounded-md border border-gray-400 focus:outline-none"
+            value={destinationQuery}
+            onChange={(e) => setDestinationQuery(e.target.value)}
           />
+          <div className="w-full h-[80%] mt-2 space-y-2">
+            {filteredSuggestions.map((suggestion, index) => (
+              <div
+                className="w-full h-[20%] bg-white rounded-md relative cursor-pointer"
+                key={index}
+                onClick={() => chooseDestinationHandler(suggestion.city)}
+              >
+                <img
+                  src={flightIcon}
+                  alt=""
+                  className="absolute left-[5%] top-[50%] translate-x-[-5%] translate-y-[-50%] h-6 w-6"
+                />
 
-          <div className="w-full h-[80%] mt-2">
-            <div className="w-full h-[20%] bg-[#f0efef] rounded-md"></div>
+                <div className="absolute left-[15%] top-[50%] translate-y-[-50%]">
+                  <p className="font-bold text-bold">{suggestion.city}</p>
+                  <p className="text-xs text-gray-500">{suggestion.airport}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       )}
 
       <Card
         className={`h-[95vh] w-[95vw] bg-white rounded-md absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-xl  ${
-          destinationForm ? "opacity-40" : ""
+          openDestination ? "opacity-40" : ""
         }`}
       >
         <div className="absolute top-[15%] w-[100%] h-[85%] flex flex-col justify-center items-center">
@@ -56,8 +126,9 @@ const Flight = () => {
             <div className="mt-[-0.5rem] w-[100%] h-[55%] flex flex-col space-y-4 relative justify-center md:w-[90%] md:ml-[5%] md:items-center md:flex-row md:space-x-[8%] md:mt-4 md:h-[30%] md:space-y-0 lg:w-[80%] lg:ml-[10%]">
               {flightDetails.map((flight) => (
                 <div
-                  className="w-[80%] ml-[10%] h-[4rem] bg-[rgba(230,230,230,0.6)] rounded-lg shadow-md relative md:w-[45%] md:ml-0"
+                  className="w-[80%] ml-[10%] h-[4rem] bg-[rgba(230,230,230,0.6)] rounded-lg shadow-md relative md:w-[45%] md:ml-0 cursor-pointer"
                   key={flight.id}
+                  onClick={() => destinationHandler(flight.way)}
                 >
                   <img
                     src={flight.icon}
@@ -85,28 +156,45 @@ const Flight = () => {
             </div>
             {/* destination details end */}
 
-            <div className="mt-2 h-[25%] w-[80%] ml-[10%] flex flex-row md:h-[30%] md:mt-2 md:items-center md:justify-center">
-              <div className="w-[50%] flex flex-col items-center space-y-2 md:w-[30%]">
+            <div className="mt-2 h-[25%] w-[80%] ml-[10%] space-x-4 flex flex-row md:h-[30%] md:mt-2 md:items-center md:justify-center">
+              <div className="w-[48%] flex flex-col items-center space-y-2 md:w-[30%]">
                 <div className="flex flex-row items-center space-x-2">
                   <FaCalendar className="text-gray-500" />
-                  <p className="uppercase text-sm font-semibold text-gray-500 ">
+                  <p className="uppercase text-sm font-semibold text-gray-500">
                     Departure
                   </p>
                 </div>
-                <p className="py-2 px-6 rounded-md bg-[rgba(230,230,230,0.6)] text-sm font-semibold text-[rgba(13,13,13,0.8)] md:text-base lg:py-4 lg:px-8">
-                  17 Sept 2021
-                </p>
+
+                <DatePicker
+                  selected={departureDate}
+                  onChange={(date) => setDepartureDate(date)}
+                  minDate={new Date()}
+                  maxDate={
+                    new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000)
+                  }
+                  dateFormat="dd MMM yyyy"
+                  className="py-2 px-2 rounded-md bg-[rgba(230,230,230,0.6)] text-sm font-semibold text-[rgba(13,13,13,0.8)] md:text-base lg:py-4 lg:px-8 cursor-pointer outline-none tracking-wider"
+                />
               </div>
-              <div className="w-[50%] flex flex-col items-center space-y-2 md:w-[30%]">
-                <div className="flex flex-row items-center space-x-2">
+              <div className="w-[48%] flex flex-col items-center space-y-2 md:w-[30%]">
+                <div className="flex flex-row items-center space-x-2 ">
                   <FaUser className="text-gray-500" />
                   <p className="uppercase text-sm font-semibold text-gray-500 ">
                     Class
                   </p>
                 </div>
-                <p className="py-2 px-6 rounded-md bg-[rgba(230,230,230,0.6)] text-sm font-semibold text-[rgba(13,13,13,0.8)] md:text-base lg:py-4 lg:px-8">
-                  Economy
-                </p>
+
+                <select
+                  value={selectedClass}
+                  onChange={(e) => setSelectedClass(e.target.value)}
+                  className="py-2 px-6 rounded-md bg-[rgba(230,230,230,0.6)]
+                  text-sm font-semibold text-[rgba(13,13,13,0.8)] md:text-base
+                  lg:py-4 lg:px-8 outline-none"
+                >
+                  <option value="Economy">Economy</option>
+                  <option value="Premium Economy">Premium Economy</option>
+                  <option value="Business">Business</option>
+                </select>
               </div>
             </div>
 
